43d9bda38b4898eb11877edc3ff500fa978ab9bb
Vivian Leung <vivz753@gmail.com>
2025-10-01 11:32:29 -0700

    Add Input story

--- component-library/src/components/Input/index.tsx
+++ component-library/src/components/Input/index.tsx
@@ -6,16 +6,31 @@ export const Input: FC<{
   value: string;
   onChange: ChangeEventHandler<HTMLInputElement> | undefined;
   placeholder?: string;
-}> = ({ className, value, onChange, placeholder }) => {
+  withCat?: boolean;
+}> = ({ className, value, onChange, placeholder, withCat }) => {
   return (
-    <input
-      onChange={onChange}
-      value={value}
-      className={clsx(
-        className,
-        "h-10 rounded-md bg-m3 p-3 text-cream placeholder:text-m1 focus:outline-cream outline-transparent outline-2"
+    <div className="group relative justify-center border max-w-max rounded-md">
+      {withCat && (
+        <div
+          className={clsx(
+            "absolute left-[50%] top-0 z-0 h-10 w-10 translate-y-0 transform opacity-0 transition-all group-focus-within:z-[2] group-focus-within:-translate-y-[70%] group-focus-within:opacity-100 group-hover:z-[2] group-hover:-translate-y-[70%] group-hover:opacity-100 sm:left-20"
+          )}>
+          <img
+            alt="typing cat"
+            src="https://media4.giphy.com/media/UQ1EI1ML2ABQdbebup/200w.webp?cid=ecf05e474pw209zhbrob0zej2huygxqrkatwltps7d75n1bu&rid=200w.webp&ct=s"
+            style={{ objectFit: "contain" }}
+          />
+        </div>
       )}
-      placeholder={placeholder}
-    />
+      <input
+        onChange={onChange}
+        value={value}
+        className={clsx(
+          className,
+          "h-10 rounded-md bg-m3 p-3 text-cream placeholder:text-m1  outline-2"
+        )}
+        placeholder={placeholder}
+      />
+    </div>
   );
 };
