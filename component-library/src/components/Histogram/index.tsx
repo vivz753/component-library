929fbb20e8b66deaeefbb1fa5333a488ffe7b50c
Vivian Leung <vivz753@gmail.com>
2025-09-30 20:11:47 -0700

    Add axes to histogram, with large data

--- component-library/src/components/Histogram/index.tsx
+++ component-library/src/components/Histogram/index.tsx
@@ -1,5 +1,9 @@
 import * as d3 from "d3";
-import { useMemo } from "react";
+import { useMemo, useEffect, useRef } from "react";
+
+const BIN_PADDING = 1;
+const MARGIN = { top: 30, right: 30, bottom: 40, left: 50 };
+const BIN_NUMBER = 70;
 
 type HistogramProps = {
   width?: number;
@@ -8,35 +12,39 @@ type HistogramProps = {
 };
 
 // const domain = [0, 20];
-const domain = [0, 10];
-// const exampleData = [0, 1, 1, 3, 4, 6, 7, 8, 2, 3, 6, 8, 9, 10, 11, 18, 13, 15, 16, 19, 20];
-const exampleData = [1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 6, 6, 9];
+// const domain = [0, 10]
+const domain = [0, 1000];
+const exampleData = [
+  0, 1, 1, 3, 4, 6, 7, 8, 2, 3, 6, 8, 9, 10, 11, 18, 13, 15, 16, 19, 20,
+];
+// const exampleData = [1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 6, 6, 9]
 // const thresholds = [0, 5, 10, 15, 20]
-const thresholds = [0, 2, 4, 6, 8, 10];
+// const thresholds = [0, 2, 4, 6, 8, 10]
 export const Histogram = ({
   width = 700,
   height = 400,
   data = exampleData,
 }: HistogramProps) => {
+  const axesRef = useRef(null);
+  const boundWidth = width - MARGIN.right - MARGIN.left;
+  const boundHeight = height - MARGIN.top - MARGIN.bottom;
   //read the data
 
   // build the scales
-  const xScale = useMemo(
-    () =>
-      d3
-        .scaleLinear()
-        .domain(domain)
-        .range([10, width - 10]),
-    [data, width]
-  );
+  const xScale = useMemo(() => {
+
+    return d3.scaleLinear().domain(domain).range([10, boundWidth]);
+  }, [data, width]);
+
+  console.log("xScale:", xScale);
 
   //build the buckets from data
   const bins = useMemo(() => {
     const binBuilder = d3
       .bin()
       .value((d) => d)
-      .domain(domain)
-      .thresholds(thresholds);
+      .domain(xScale?.domain()) // why use domain here instead of regular domain?
+      .thresholds(xScale.ticks(BIN_NUMBER)); // why use ticks here?
 
     return binBuilder(data);
   }, [xScale]);
@@ -44,11 +52,26 @@ export const Histogram = ({
   const yScale = useMemo(() => {
     const max = Math.max(...bins.map((b) => b?.length));
 
-    return d3.scaleLinear().range([height, 0]).domain([0, max]);
+    return d3.scaleLinear().range([boundHeight, 0]).domain([0, max]).nice(); // what's nice() do?
   }, [data, height]);
 
-  //build the rectangels
-  const rects = bins.map((b, i) => {
+  // Render the axes
+  useEffect(() => {
+    const svgElement = d3.select(axesRef.current);
+    svgElement.selectAll("*").remove();
+
+    const xAxisGenerator = d3.axisBottom(xScale);
+    svgElement
+      .append("g")
+      .attr("transform", "translate(0," + boundHeight + ")")
+      .call(xAxisGenerator);
+
+    const yAxisGenerator = d3.axisLeft(yScale);
+    svgElement.append("g").call(yAxisGenerator);
+  }, [xScale, yScale, boundHeight]);
+
+  // Construct the bars
+  const bars = bins.map((b, i) => {
     console.log("bin", b, b.length);
 
     return b.x0 !== undefined && b.x1 !== undefined ? (
@@ -56,17 +79,28 @@ export const Histogram = ({
         key={i}
         fill="var(--color-m1)"
         stroke="var(--color-m3)"
-        x={xScale(b.x0)}
-        width={xScale(b.x1) - xScale(b.x0)}
+        x={xScale(b.x0) + BIN_PADDING / 2}
+        width={xScale(b.x1) - xScale(b.x0) - BIN_PADDING}
         y={yScale(b.length)}
-        height={height - yScale(b.length)}
+        height={boundHeight - yScale(b.length)}
       />
     ) : null;
   });
 
   return (
-    <svg className="" width={width} height={height}>
-      {rects}
+    <svg className="border border-green-200" width={width} height={height}>
+      <g
+        width={boundWidth}
+        height={boundHeight}
+        transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}>
+        {bars}
+      </g>
+      <g
+        width={boundWidth}
+        height={boundHeight}
+        ref={axesRef}
+        transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
+      />
     </svg>
   );
 };
