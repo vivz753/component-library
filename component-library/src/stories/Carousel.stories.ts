c3bc99e335342c229d605c49e4b4e6bf51720425
Vivian Leung <vivz753@gmail.com>
2025-10-01 11:33:16 -0700

    Add medium Carousel story

--- component-library/src/stories/Carousel.stories.ts
+++ component-library/src/stories/Carousel.stories.ts
@@ -1,19 +1,19 @@
-import type { Meta, StoryObj } from '@storybook/react-vite';
+import type { Meta, StoryObj } from "@storybook/react-vite";
 
-import { fn } from 'storybook/test';
+import { fn } from "storybook/test";
 
-import { Carousel } from '../components/Carousel';
+import { Carousel } from "../components/Carousel";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Carousel',
+  title: "Example/Carousel",
   component: Carousel,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   // argTypes: {
   //   backgroundColor: { control: 'color' },
@@ -26,14 +26,18 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Large: Story = {
-  args: {
-    size: 'large',
-  },
-};
-
 export const Small: Story = {
   args: {
-    size: 'small',
+    size: "small",
+  },
+};
+export const Medium: Story = {
+  args: {
+    size: "medium",
+  },
+};
+export const Large: Story = {
+  args: {
+    size: "large",
   },
 };
